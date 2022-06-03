3ab9ed0fb502a2eb69877f82149447c5f1227f2d
James Hughes <james@pyrosoftsolutions.co.uk>
2022-06-03 16:44:14 +0100

    Fixed type extensions and added tests

--- src/DISCOSweb-Sdk/DISCOSweb-Sdk/Extensions/TypeExtensions.cs
+++ src/DISCOSweb-Sdk/DISCOSweb-Sdk/Extensions/TypeExtensions.cs
@@ -1,12 +1,30 @@
+using System.Reflection;
+
 namespace DISCOSweb_Sdk.Extensions;
 
 internal static class TypeExtensions
 {
 	internal static void EnsureFieldExists(this Type type, string fieldName)
 	{
-		if (type.GetField(fieldName) is null && type.GetProperty(fieldName) is not null)
+		if (type.GetField(fieldName) is null && type.GetProperty(fieldName) is null)
 		{
 			throw new MissingFieldException($"Field ({fieldName}) does not exist on type {type.Name}");
+		}
+	}
+
+	internal static void EnsureFieldIsOfType(this Type objectType, string fieldName, Type fieldType)
+	{
+		objectType.EnsureFieldExists(fieldName);
+		FieldInfo? fieldInfo = objectType.GetField(fieldName);
+		if (fieldInfo is not null && fieldInfo.FieldType != fieldType)
+		{
+			throw new MissingFieldException($"Field ({fieldName}) is not of type ({fieldType})");
+		}
+
+		PropertyInfo? propInfo = objectType.GetProperty(fieldName);
+		if (propInfo is not null && propInfo.PropertyType != fieldType)
+		{
+			throw new MissingFieldException($"Field ({fieldName}) is not of type ({fieldType})");
 		}
 	}
 	
