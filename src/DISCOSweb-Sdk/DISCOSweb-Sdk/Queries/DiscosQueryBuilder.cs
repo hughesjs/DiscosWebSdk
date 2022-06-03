961108b119a7cf003dc39cb642aac60658587656
James Hughes <james@pyrosoftsolutions.co.uk>
2022-06-03 19:48:09 +0100

    Added Discos Query Builder

---
+++ src/DISCOSweb-Sdk/DISCOSweb-Sdk/Queries/DiscosQueryBuilder.cs
@@ -0,0 +1,130 @@
+using System.Text;
+using DISCOSweb_Sdk.Extensions;
+
+namespace DISCOSweb_Sdk.Queries;
+
+public class DiscosQueryBuilder<TObject>: IDiscosQueryBuilder<TObject>
+{
+	private List<FilterDefinition> _filterDefinitions = new();
+	private List<string> _includes = new();
+	private int? _numPages;
+	private int? _pageNum;
+
+	public DiscosQueryBuilder()
+	{
+		Reset();
+	}
+
+	public IDiscosQueryBuilder<TObject> AddFilter(FilterDefinition filterDefinition) 
+	{
+		_filterDefinitions.Add(filterDefinition);
+		return this;
+	}
+
+	public IDiscosQueryBuilder<TObject> AddInclude(string fieldName)
+	{
+		typeof(TObject).EnsureFieldExists(fieldName);
+		_includes.Add(fieldName);
+		return this;
+	}
+
+	public IDiscosQueryBuilder<TObject> AddAllIncludes() => throw new NotImplementedException();
+
+	public IDiscosQueryBuilder<TObject> AddPageSize(int numPages)
+	{
+		_numPages = numPages;
+		return this;
+	}
+
+	public IDiscosQueryBuilder<TObject> PageNum(int pageNum)
+	{
+		_pageNum = pageNum;
+		return this;
+	}
+
+	public IDiscosQueryBuilder<TObject> Reset()
+	{
+		_filterDefinitions = new();
+		_includes = new();
+		_numPages = null;
+		_pageNum = null;
+		return this;
+	}
+
+	public string GetQueryString()
+	{
+		void AppendJoiningChar(StringBuilder builder, ref bool isFirst)
+		{
+			builder.Append(isFirst ? '?' : '&');
+			isFirst = false;
+		}
+		StringBuilder builder = new();
+		bool isFirst = true;
+		if (_filterDefinitions.Count > 0)
+		{
+			AppendJoiningChar(builder, ref isFirst);
+			AddFilterString(builder);
+		}
+
+		if (_includes.Count > 0)
+		{
+			AppendJoiningChar(builder, ref isFirst);
+			AddIncludeString(builder);
+		}
+
+		if (_pageNum is not null)
+		{
+			AppendJoiningChar(builder, ref isFirst);
+			AddPageNumber(builder);
+		}
+
+		if (_numPages is not null)
+		{
+			AppendJoiningChar(builder, ref isFirst);
+			AddNumPages(builder);
+		}
+		return builder.ToString();
+	}
+
+	private void AddFilterString(StringBuilder builder)
+	{
+		builder.Append("filter=");
+
+		foreach (FilterDefinition f in _filterDefinitions)
+		{
+			builder.Append(f);
+			;
+
+		}
+			
+	}
+
+	private void AddIncludeString(StringBuilder builder)
+	{
+		builder.Append("include=");
+		_includes.ForEach(i => builder.Append(i));
+	}
+
+	private void AddPageNumber(StringBuilder builder)
+	{
+		builder.Append($"page[number]={_pageNum}");
+	}
+
+	private void AddNumPages(StringBuilder builder)
+	{
+		builder.Append($"page[size]={_numPages}");
+	}
+}
+
+
+public interface IDiscosQueryBuilder<TObject>
+{
+	public IDiscosQueryBuilder<TObject> AddFilter(FilterDefinition filterDefinition);
+	public IDiscosQueryBuilder<TObject> AddInclude(string fieldName);
+	public IDiscosQueryBuilder<TObject> AddAllIncludes();
+	public IDiscosQueryBuilder<TObject> AddPageSize(int numPages);
+	public IDiscosQueryBuilder<TObject> PageNum(int pageNum);
+	public IDiscosQueryBuilder<TObject> Reset();
+	public string GetQueryString();
+}
+
