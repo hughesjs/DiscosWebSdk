0756a5b3b3951ec4040bdbd55d81a8e987d6d1df
James Hughes <james@pyrosoftsolutions.co.uk>
2022-06-03 17:32:57 +0100

    Added option to ignore nullability on type in type extensions

--- src/DISCOSweb-Sdk/DISCOSweb-Sdk.Tests/Extensions/TypeExtensionsTests.cs
+++ src/DISCOSweb-Sdk/DISCOSweb-Sdk.Tests/Extensions/TypeExtensionsTests.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Net.Http;
+using System.Security.Authentication.ExtendedProtection;
 using DISCOSweb_Sdk.Enums;
 using DISCOSweb_Sdk.Extensions;
 using DISCOSweb_Sdk.Models.ResponseModels.DiscosObjects;
@@ -51,7 +54,17 @@ public class TypeExtensionsTests
 	[InlineData(typeof(DiscosObject), typeof(Reentry), nameof(DiscosObject.Reentry))]
 	public void CanEnsureFieldIsOfType(Type objectType, Type fieldType, string fieldName)
 	{
-		Should.NotThrow(() => objectType.EnsureFieldIsOfType(fieldName, fieldType));
+		Should.NotThrow(() => objectType.EnsureFieldIsOfType(fieldName, fieldType, true));
+	}
+
+	[Theory]
+	[InlineData(typeof(DiscosObject), typeof(float), nameof(DiscosObject.Depth))]
+	[InlineData(typeof(DiscosObject), typeof(float), nameof(DiscosObject.Height))]
+	[InlineData(typeof(DiscosObject), typeof(float), nameof(DiscosObject.Length))]
+	[InlineData(typeof(DiscosObject), typeof(float), nameof(DiscosObject.Mass))]
+	public void CanEnsureFieldIsOfTypeIgnoringNullability(Type objectType, Type fieldType, string fieldName)
+	{
+		Should.NotThrow(() => objectType.EnsureFieldIsOfType(fieldName, fieldType, false));
 	}
 
 	[Theory]
@@ -63,5 +76,63 @@ public class TypeExtensionsTests
 	{
 		Should.Throw<MissingFieldException>(() => objectType.EnsureFieldIsOfType(fieldName, fieldType));
 	}
+
+	[Theory]
+	[InlineData(typeof(byte), true)]
+	[InlineData(typeof(byte?), true)]
+	[InlineData(typeof(float), true)]
+	[InlineData(typeof(float?), true)]
+	[InlineData(typeof(double), true)]
+	[InlineData(typeof(double?), true)]
+	[InlineData(typeof(int), true)]
+	[InlineData(typeof(int?), true)]
+	[InlineData(typeof(long), true)]
+	[InlineData(typeof(long?), true)]
+	[InlineData(typeof(uint), true)]
+	[InlineData(typeof(uint?), true)]
+	[InlineData(typeof(ulong), true)]
+	[InlineData(typeof(ulong?), true)]
+	[InlineData(typeof(string), false)]
+	[InlineData(typeof(object), false)]
+	[InlineData(typeof(DiscosObject), false)]
+	[InlineData(typeof(Launch), false)]
+	[InlineData(typeof(Reentry), false)]
+	public void CanDetermineWhetherNumericType(Type t, bool isNumeric)
+	{
+		bool res = t.IsNumericType();
+		if (isNumeric)
+		{
+			res.ShouldBeTrue();
+		}
+		else
+		{
+			res.ShouldBeFalse();
+		}
+	}
 	
+	[Theory]
+	[InlineData(typeof(string[]), true)]
+	[InlineData(typeof(List<string>), true)]
+	[InlineData(typeof(Dictionary<object,double>), true)]
+	[InlineData(typeof(ConcurrentBag<object>), true)]
+	[InlineData(typeof(string[][][][][]), true)]
+	[InlineData(typeof(ServiceNameCollection), true)]
+	[InlineData(typeof(string), false)]
+	[InlineData(typeof(object), false)]
+	[InlineData(typeof(Launch), false)]
+	[InlineData(typeof(Reentry), false)]
+	[InlineData(typeof(DiscosObject), false)]
+	public void CanDetermineWhetherCollectionType(Type t, bool isNumeric)
+	{
+		bool res = t.IsCollectionType();
+		if (isNumeric)
+		{
+			res.ShouldBeTrue();
+		}
+		else
+		{
+			res.ShouldBeFalse();
+		}
+	}
+
 }
--- src/DISCOSweb-Sdk/DISCOSweb-Sdk/Extensions/TypeExtensions.cs
+++ src/DISCOSweb-Sdk/DISCOSweb-Sdk/Extensions/TypeExtensions.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Reflection;
 
 namespace DISCOSweb_Sdk.Extensions;
@@ -12,20 +13,62 @@ internal static class TypeExtensions
 		}
 	}
 
-	internal static void EnsureFieldIsOfType(this Type objectType, string fieldName, Type fieldType)
+	internal static void EnsureFieldIsOfType(this Type objectType, string fieldName, Type fieldType, bool checkNullability = false)
 	{
+		
 		objectType.EnsureFieldExists(fieldName);
 		FieldInfo? fieldInfo = objectType.GetField(fieldName);
-		if (fieldInfo is not null && fieldInfo.FieldType != fieldType)
+		Type? objectFieldType = fieldInfo?.FieldType;
+		
+		PropertyInfo? propInfo = objectType.GetProperty(fieldName);
+		Type? objectPropType = propInfo?.PropertyType;
+		
+		if (!checkNullability)
+		{
+			fieldType = Nullable.GetUnderlyingType(fieldType) ?? fieldType;
+			if (objectFieldType is not null)
+			{
+				objectFieldType = Nullable.GetUnderlyingType(objectFieldType) ?? objectFieldType;
+			}
+			if (objectPropType is not null)
+			{
+				objectPropType = Nullable.GetUnderlyingType(objectPropType) ?? objectPropType;
+			}
+		}
+		
+		if (objectFieldType is not null && objectFieldType != fieldType)
 		{
 			throw new MissingFieldException($"Field ({fieldName}) is not of type ({fieldType})");
 		}
 
-		PropertyInfo? propInfo = objectType.GetProperty(fieldName);
-		if (propInfo is not null && propInfo.PropertyType != fieldType)
+		
+		if (objectPropType is not null && objectPropType != fieldType)
 		{
 			throw new MissingFieldException($"Field ({fieldName}) is not of type ({fieldType})");
 		}
 	}
 	
+	public static bool IsNumericType(this Type t)
+	{
+		t = Nullable.GetUnderlyingType(t) ?? t; // If nullable, get underlying type
+		switch (Type.GetTypeCode(t))
+		{
+			case TypeCode.Byte:
+			case TypeCode.SByte:
+			case TypeCode.UInt16:
+			case TypeCode.UInt32:
+			case TypeCode.UInt64:
+			case TypeCode.Int16:
+			case TypeCode.Int32:
+			case TypeCode.Int64:
+			case TypeCode.Decimal:
+			case TypeCode.Double:
+			case TypeCode.Single:
+				return true;
+			default:
+				return false;
+		}
+	}
+
+	public static bool IsCollectionType(this Type t) => t != typeof(string) && t.GetInterface(nameof(IEnumerable)) is not null;
 }
