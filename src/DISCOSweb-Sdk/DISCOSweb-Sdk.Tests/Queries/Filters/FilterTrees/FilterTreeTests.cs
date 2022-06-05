831c00ac24016091b99138d89a23922aad35e9e0
James Hughes <james@pyrosoftsolutions.co.uk>
2022-06-05 20:04:41 +0100

    Commented out some query builder stuff until I've tested the new tree

--- src/DISCOSweb-Sdk/DISCOSweb-Sdk.Tests/Queries/Filters/FilterTrees/FilterTreeTests.cs
+++ src/DISCOSweb-Sdk/DISCOSweb-Sdk.Tests/Queries/Filters/FilterTrees/FilterTreeTests.cs
@@ -1,7 +1,11 @@
+using System;
+using System.Linq;
+using AutoFixture.Kernel;
 using DISCOSweb_Sdk.Enums;
 using DISCOSweb_Sdk.Models.ResponseModels.DiscosObjects;
 using DISCOSweb_Sdk.Queries.Filters;
-using DISCOSweb_Sdk.Queries.Filters.Tree;
+using DISCOSweb_Sdk.Queries.Filters.FilterTree;
+using DISCOSweb_Sdk.Queries.Filters.FilterTree.Nodes;
 using Shouldly;
 using Xunit;
 
@@ -9,34 +13,77 @@ namespace DISCOSweb_Sdk.Tests.Queries.Filters.FilterTrees;
 
 public class FilterTreeTests
 {
-	[Fact]
-	public void NewTreeHasZeroNodes() // We don't count root node
-	{
-		FilterTree tree = new();
-		tree.CountNodes().ShouldBe(0);
-	}
+	// [Fact]
+	// public void NewTreeHasZeroNodes() // We don't count root node
+	// {
+	// 	FilterTree tree = new();
+	// 	tree.CountNodes().ShouldBe(0);
+	// }
+	//
+	// [Fact]
+	// public void CanCountTwoLayersOfNodes()
+	// {
+	// 	FilterTree tree = new();
+	// 	FilterNode andNode = tree.Root.AddChild(new OperationNode(FilterOperation.And));
+	// 	andNode.AddChild(new DefinitionNode(new FilterDefinition<DiscosObject,string>(nameof(DiscosObject.Name), "International", DiscosFunction.Contains)));
+	// 	andNode.AddChild(new DefinitionNode(new FilterDefinition<DiscosObject,float>(nameof(DiscosObject.Height), 0.5f, DiscosFunction.GreaterThan)));
+	// 	tree.CountNodes().ShouldBe(3);
+	// }
+	//
+	// [Fact]
+	// public void CanCountThreeLayersOfNodes()
+	// {
+	// 	FilterTree tree = new();
+	// 	FilterNode topAnd = tree.Root.AddChild(new OperationNode(FilterOperation.And));
+	// 	FilterNode l2Or = topAnd.AddChild(new OperationNode(FilterOperation.Or));
+	// 	FilterNode l2And = topAnd.AddChild(new OperationNode(FilterOperation.And));
+	// 	l2Or.AddChild(new OperationNode(FilterOperation.Or));
+	// 	l2Or.AddChild(new OperationNode(FilterOperation.Or));
+	// 	l2And.AddChild(new OperationNode(FilterOperation.And));
+	// 	l2And.AddChild(new OperationNode(FilterOperation.And));
+	// 	tree.CountNodes().ShouldBe(7);
+	// }
+	//
+	// [Fact]
+	// public void CanBuildSingleOperationTree()
+	// {
+	// 	FilterTree tree = new();
+	// 	FilterDefinition left = new FilterDefinition<DiscosObject, string>(nameof(DiscosObject.Id), "123", DiscosFunction.Equal);
+	// 	FilterDefinition right = new FilterDefinition<DiscosObject, string>(nameof(DiscosObject.Name), "321", DiscosFunction.NotEqual);
+	// 	tree.AddFilter(left);
+	// 	tree.AddAnd();
+	// 	tree.AddFilter(right);
+	// 	
+	// 	tree.CountNodes().ShouldBe(3);
+	// 	tree.Root.Children.First().ShouldBeOfType<OperationNode>();
+	// 	tree.Root.Children.First().Children.ShouldAllBe(node => node is DefinitionNode);
+	// }
+	//
+	// [Fact]
+	// public void CanAddSingleDefinitionToRoot()
+	// {
+	// 	FilterTree tree = new();
+	// 	tree.AddFilter(new FilterDefinition<DiscosObject, string>(nameof(DiscosObject.Id), "123", DiscosFunction.Equal));
+	// }
+	//
+	// [Fact]
+	// public void CanBuildSingleCompoundNodeTree()
+	// {
+	// 	FilterTree tree = new();
+	// 	FilterDefinition left1 = new FilterDefinition<DiscosObject, string>(nameof(DiscosObject.Id), "123", DiscosFunction.Equal);
+	// 	FilterDefinition right1 = new FilterDefinition<DiscosObject, string>(nameof(DiscosObject.Name), "321", DiscosFunction.NotEqual);
+	// 	FilterDefinition left2 = new FilterDefinition<DiscosObject, string>(nameof(DiscosObject.Id), "333", DiscosFunction.Equal);
+	// 	FilterDefinition right2 = new FilterDefinition<DiscosObject, string>(nameof(DiscosObject.Name), "666", DiscosFunction.NotEqual);
+	// 	tree.AddFilter(left1);
+	// 	tree.AddOr();
+	// 	tree.AddFilter(right1);
+	// 	tree.AddAnd();
+	// 	tree.AddFilter(left2);
+	// 	tree.AddOr();
+	// 	tree.AddFilter(right2);
+	//
+	// 	
+	//
+	// }
 
-	[Fact]
-	public void CanCountTwoLayersOfNodes()
-	{
-		FilterTree tree = new();
-		FilterNode andNode = tree.RootNode.AddChild(new OperationNode(NodeOperation.And));
-		andNode.AddChild(new DefinitionNode(new FilterDefinition<DiscosObject,string>(nameof(DiscosObject.Name), "International", DiscosFunction.Contains)));
-		andNode.AddChild(new DefinitionNode(new FilterDefinition<DiscosObject,float>(nameof(DiscosObject.Height), 0.5f, DiscosFunction.GreaterThan)));
-		tree.CountNodes().ShouldBe(3);
-	}
-
-	[Fact]
-	public void CanCountThreeLayersOfNodes()
-	{
-		FilterTree tree = new();
-		FilterNode topAnd = tree.RootNode.AddChild(new OperationNode(NodeOperation.And));
-		FilterNode l2Or = topAnd.AddChild(new OperationNode(NodeOperation.Or));
-		FilterNode l2And = topAnd.AddChild(new OperationNode(NodeOperation.And));
-		l2Or.AddChild(new OperationNode(NodeOperation.Or));
-		l2Or.AddChild(new OperationNode(NodeOperation.Or));
-		l2And.AddChild(new OperationNode(NodeOperation.And));
-		l2And.AddChild(new OperationNode(NodeOperation.And));
-		tree.CountNodes().ShouldBe(7);
-	}
 }
--- src/DISCOSweb-Sdk/DISCOSweb-Sdk/Queries/DiscosQueryBuilder.cs
+++ src/DISCOSweb-Sdk/DISCOSweb-Sdk/Queries/DiscosQueryBuilder.cs
@@ -1,116 +1,133 @@
 using System.Text;
-using DISCOSweb_Sdk.Exceptions;
+using DISCOSweb_Sdk.Enums;
 using DISCOSweb_Sdk.Extensions;
 using DISCOSweb_Sdk.Queries.Filters;
-using DISCOSweb_Sdk.Queries.Filters.Tree;
+using DISCOSweb_Sdk.Queries.Filters.FilterTree;
 
 namespace DISCOSweb_Sdk.Queries;
 
-public class DiscosQueryBuilder<TObject>: IDiscosQueryBuilder<TObject>
-{
-	private FilterTree _filterDefinitions = new();
-	private List<string> _includes = new();
-	private int? _numPages;
-	private int? _pageNum;
-
-	public DiscosQueryBuilder()
-	{
-		Reset();
-	}
-
-	public IDiscosQueryBuilder<TObject> AddFilter(FilterDefinition filterDefinition)
-	{
-		throw new NotImplementedException();
-		return this;
-	}
-
-	public IDiscosQueryBuilder<TObject> AddInclude(string fieldName)
-	{
-		typeof(TObject).EnsureFieldExists(fieldName);
-		_includes.Add(fieldName);
-		return this;
-	}
-
-	public IDiscosQueryBuilder<TObject> AddAllIncludes() => throw new NotImplementedException();
-
-	public IDiscosQueryBuilder<TObject> AddPageSize(int numPages)
-	{
-		_numPages = numPages;
-		return this;
-	}
-
-	public IDiscosQueryBuilder<TObject> PageNum(int pageNum)
-	{
-		_pageNum = pageNum;
-		return this;
-	}
-
-	public IDiscosQueryBuilder<TObject> Reset()
-	{
-		_filterDefinitions = new();
-		_includes = new();
-		_numPages = null;
-		_pageNum = null;
-		return this;
-	}
-
-	public string GetQueryString()
-	{
-		void AppendJoiningChar(StringBuilder builder, ref bool isFirst)
-		{
-			builder.Append(isFirst ? '?' : '&');
-			isFirst = false;
-		}
-		StringBuilder builder = new();
-		bool isFirst = true;
-		if (_filterDefinitions.CountNodes() > 0)
-		{
-			AppendJoiningChar(builder, ref isFirst);
-			AddFilterString(builder);
-		}
-
-		if (_includes.Count > 0)
-		{
-			AppendJoiningChar(builder, ref isFirst);
-			AddIncludeString(builder);
-		}
-
-		if (_pageNum is not null)
-		{
-			AppendJoiningChar(builder, ref isFirst);
-			AddPageNumber(builder);
-		}
-
-		if (_numPages is not null)
-		{
-			AppendJoiningChar(builder, ref isFirst);
-			AddNumPages(builder);
-		}
-		return builder.ToString();
-	}
-	
-	private void AddFilterString(StringBuilder builder)
-	{
-		builder.Append("filter=");
-		throw new NotImplementedException();
-	}
-
-	private void AddIncludeString(StringBuilder builder)
-	{
-		builder.Append("include=");
-		_includes.ForEach(i => builder.Append(i));
-	}
-
-	private void AddPageNumber(StringBuilder builder)
-	{
-		builder.Append($"page[number]={_pageNum}");
-	}
-
-	private void AddNumPages(StringBuilder builder)
-	{
-		builder.Append($"page[size]={_numPages}");
-	}
-}
+// public class DiscosQueryBuilder<TObject>: IDiscosQueryBuilder<TObject>
+// {
+// 	private FilterTree _filterDefinitions;
+// 	private FilterNode _currentNode;
+// 	private List<string> _includes;
+// 	private int? _numPages;
+// 	private int? _pageNum;
+//
+// 	public DiscosQueryBuilder()
+// 	{
+// 		_filterDefinitions = new();
+// 		_includes = new();
+// 		_currentNode = _filterDefinitions.Root;
+// 		Reset();
+// 	}
+//
+// 	public IDiscosQueryBuilder<TObject> AddFilter(FilterDefinition filterDefinition)
+// 	{
+// 		_filterDefinitions.AddFilter(filterDefinition);
+// 		return this;
+// 	}
+//
+// 	public IDiscosQueryBuilder<TObject> AddInclude(string fieldName)
+// 	{
+// 		typeof(TObject).EnsureFieldExists(fieldName);
+// 		_includes.Add(fieldName);
+// 		return this;
+// 	}
+//
+// 	public IDiscosQueryBuilder<TObject> AddAllIncludes() => throw new NotImplementedException();
+//
+// 	public IDiscosQueryBuilder<TObject> AddPageSize(int numPages)
+// 	{
+// 		_numPages = numPages;
+// 		return this;
+// 	}
+//
+// 	public IDiscosQueryBuilder<TObject> PageNum(int pageNum)
+// 	{
+// 		_pageNum = pageNum;
+// 		return this;
+// 	}
+//
+// 	public IDiscosQueryBuilder<TObject> And()
+// 	{
+// 		_filterDefinitions.AddAnd();
+// 		return this;
+// 	}
+//
+// 	public IDiscosQueryBuilder<TObject> Or()
+// 	{
+// 		_filterDefinitions.AddOr();
+// 		return this;
+// 	}
+//
+// 	public IDiscosQueryBuilder<TObject> Reset()
+// 	{
+// 		_filterDefinitions = new();
+// 		_currentNode = _filterDefinitions.Root;
+// 		_includes = new();
+// 		_numPages = null;
+// 		_pageNum = null;
+// 		return this;
+// 	}
+//
+// 	public string GetQueryString()
+// 	{
+// 		void AppendJoiningChar(StringBuilder builder, ref bool isFirst)
+// 		{
+// 			builder.Append(isFirst ? '?' : '&');
+// 			isFirst = false;
+// 		}
+// 		StringBuilder builder = new();
+// 		bool isFirst = true;
+// 		if (_filterDefinitions.CountNodes() > 0)
+// 		{
+// 			AppendJoiningChar(builder, ref isFirst);
+// 			AddFilterString(builder);
+// 		}
+//
+// 		if (_includes.Count > 0)
+// 		{
+// 			AppendJoiningChar(builder, ref isFirst);
+// 			AddIncludeString(builder);
+// 		}
+//
+// 		if (_pageNum is not null)
+// 		{
+// 			AppendJoiningChar(builder, ref isFirst);
+// 			AddPageNumber(builder);
+// 		}
+//
+// 		if (_numPages is not null)
+// 		{
+// 			AppendJoiningChar(builder, ref isFirst);
+// 			AddNumPages(builder);
+// 		}
+// 		return builder.ToString();
+// 	}
+// 	
+// 	private void AddFilterString(StringBuilder builder)
+// 	{
+// 		builder.Append("filter=");
+// 		throw new NotImplementedException();
+// 	}
+//
+// 	private void AddIncludeString(StringBuilder builder)
+// 	{
+// 		builder.Append("include=");
+// 		_includes.ForEach(i => builder.Append(i));
+// 	}
+//
+// 	private void AddPageNumber(StringBuilder builder)
+// 	{
+// 		builder.Append($"page[number]={_pageNum}");
+// 	}
+//
+// 	private void AddNumPages(StringBuilder builder)
+// 	{
+// 		builder.Append($"page[size]={_numPages}");
+// 	}
+// }
 
 
 public interface IDiscosQueryBuilder<TObject>
@@ -120,6 +137,9 @@ public interface IDiscosQueryBuilder<TObject>
 	public IDiscosQueryBuilder<TObject> AddAllIncludes();
 	public IDiscosQueryBuilder<TObject> AddPageSize(int numPages);
 	public IDiscosQueryBuilder<TObject> PageNum(int pageNum);
+	public IDiscosQueryBuilder<TObject> And();
+	public IDiscosQueryBuilder<TObject> Or();
+	
 	public IDiscosQueryBuilder<TObject> Reset();
 	public string GetQueryString();
 }
