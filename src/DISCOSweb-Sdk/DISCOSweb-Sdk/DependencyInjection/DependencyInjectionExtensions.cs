22904885bf525f5d9e6b72ce9ab0c57abcbcec84
James Hughes <james@pyrosoftsolutions.co.uk>
2022-06-08 20:25:48 +0100

    Added DI for DiscosClient

--- src/DISCOSweb-Sdk/DISCOSweb-Sdk/DependencyInjection/DependencyInjectionExtensions.cs
+++ src/DISCOSweb-Sdk/DISCOSweb-Sdk/DependencyInjection/DependencyInjectionExtensions.cs
@@ -1,3 +1,5 @@
+using DISCOSweb_Sdk.Clients;
+using DISCOSweb_Sdk.Exceptions.DependencyInjection;
 using DISCOSweb_Sdk.Interfaces.Queries;
 using DISCOSweb_Sdk.Options;
 using DISCOSweb_Sdk.Queries;
@@ -9,8 +11,23 @@ public static class DependencyInjectionExtensions
 {
 	public static void AddDiscosServices(this IServiceCollection services, IConfiguration configuration)
 	{
-		//TODO - Add HTTP client
+		IConfigurationSection configSection = configuration.GetSection(nameof(DiscosOptions));
+		DiscosOptions         opt           = configSection.Get<DiscosOptions>();
+
+		if (opt.DiscosApiKey is null || opt.DiscosApiUrl is null)
+		{
+			throw new InvalidDiscosConfigurationException("DISCOS API key and URL not set");
+		}
+		
+		services.Configure<DiscosOptions>(configSection);
+		
 		services.AddTransient(typeof(IDiscosQueryBuilder<>), typeof(DiscosQueryBuilder<>));
-		services.Configure<DiscosOptions>(configuration.GetSection(nameof(DiscosOptions)));
+
+		services.AddHttpClient<DiscosClient>(c =>
+											 {
+												 c.BaseAddress                         = new(opt.DiscosApiUrl);
+												 c.DefaultRequestHeaders.Authorization = new("bearer", opt.DiscosApiKey);
+											 });
+		
 	}
 }
---
+++ src/DISCOSweb-Sdk/DISCOSweb-Sdk/Exceptions/DependencyInjection/InvalidDiscosConfigurationException.cs
@@ -0,0 +1,6 @@
+namespace DISCOSweb_Sdk.Exceptions.DependencyInjection;
+
+public class InvalidDiscosConfigurationException: Exception
+{
+	public InvalidDiscosConfigurationException(string message): base(message){}
+}
