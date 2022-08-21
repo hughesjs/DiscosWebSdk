582209bee9863e84a4cc9f1a67edf7092e71ee5f
James Hughes <james.hughes@codurance.com>
2022-08-21 17:27:45 +0100

    refactor: Refactored the DiscosQueryBuilder to make the generic version a thin wrapper around the non-generic implementation

--- src/DiscosWebSdk/DiscosWebSdk/Interfaces/Queries/IDiscosQueryBuilder.cs
+++ src/DiscosWebSdk/DiscosWebSdk/Interfaces/Queries/IDiscosQueryBuilder.cs
@@ -4,18 +4,21 @@ namespace DiscosWebSdk.Interfaces.Queries;
 
 public interface IDiscosQueryBuilder<TObject> : IDiscosQueryBuilder where TObject : notnull
 {
-	public IDiscosQueryBuilder<TObject> AddFilter(FilterDefinition filterDefinition);
-	public IDiscosQueryBuilder<TObject> And();
-	public IDiscosQueryBuilder<TObject> Or();
-	public IDiscosQueryBuilder<TObject> AddInclude(string fieldName);
-	public IDiscosQueryBuilder<TObject> AddAllIncludes();
+	public new IDiscosQueryBuilder<TObject> And();
+	public new IDiscosQueryBuilder<TObject> Or();
 	public new IDiscosQueryBuilder<TObject> AddPageSize(int numPages);
 	public new IDiscosQueryBuilder<TObject> AddPageNum(int  pageNum);
 	public new IDiscosQueryBuilder<TObject> Reset();
+	public IDiscosQueryBuilder<TObject> AddInclude(string fieldName);
+	public IDiscosQueryBuilder<TObject> AddAllIncludes();
+	public IDiscosQueryBuilder<TObject> AddFilter(FilterDefinition filterDefinition);
 }
 
 public interface IDiscosQueryBuilder
 {
+	public IDiscosQueryBuilder AddFilter(Type t, FilterDefinition filterDefinition);
+	public IDiscosQueryBuilder And();
+	public IDiscosQueryBuilder Or();
 	public IDiscosQueryBuilder AddPageSize(int numPages);
 	public IDiscosQueryBuilder AddPageNum(int  pageNum);
 	public IDiscosQueryBuilder AddAllIncludes(Type t);
